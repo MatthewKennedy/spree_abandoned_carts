1e2fbc84b6806571a41b1af58e56244894f6bbb8
Matthew Kennedy <m.kennedy@me.com>
2019-06-18 22:39:54 +0100

    use beter default  configs

--- lib/spree_abandoned_carts/configuration.rb
+++ lib/spree_abandoned_carts/configuration.rb
@@ -1,7 +1,7 @@
 module SpreeAbandonedCarts
   class Configuration < Spree::Preferences::Configuration
     preference :abandoned_states, :array, default: [:cart, :address, :delivery, :payment, :confirm]
-    preference :abandoned_after_minutes, :integer, default: 24.hours / 1.minute
-    preference :worker_frequency_minutes, :integer, default: 30.minutes
+    preference :abandoned_after_minutes, :integer, default: 10.hours / 1.minute
+    preference :worker_frequency_minutes, :integer, default: 3.minutes
   end
 end
