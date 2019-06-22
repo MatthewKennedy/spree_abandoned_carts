2a8538b7a672373acba54c7844fddbd3087d9296
Matthew Kennedy <m.kennedy@me.com>
2019-06-22 20:34:35 +0100

    try

--- app/workers/abandoned_cart_worker.rb
+++ app/workers/abandoned_cart_worker.rb
@@ -4,11 +4,5 @@ class AbandonedCartWorker
 
   def perform
     AbandonedCartService.perform
-
-    if self.class.respond_to?(:perform_in)
-      next_run = SpreeAbandonedCarts::Config.worker_frequency_minutes
-      self.class.perform_in(next_run.minutes) if next_run > 0
-    end
-
   end
 end
