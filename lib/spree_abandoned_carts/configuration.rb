730c06ebc210a405f25f54e09132458209b48069
Matthew Kennedy <m.kennedy@me.com>
2019-06-22 20:02:27 +0100

    update

--- lib/spree_abandoned_carts/configuration.rb
+++ lib/spree_abandoned_carts/configuration.rb
@@ -2,6 +2,6 @@ module SpreeAbandonedCarts
   class Configuration < Spree::Preferences::Configuration
     preference :abandoned_states, :array, default: [:cart, :address, :delivery, :payment, :confirm]
     preference :abandoned_after_minutes, :integer, default: 10.hours / 1.minute
-    preference :worker_frequency_minutes, :integer, default: 3.minutes
+    preference :worker_frequency_minutes, :integer, default: 30.minutes
   end
 end
